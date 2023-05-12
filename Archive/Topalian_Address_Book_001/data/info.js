78e144860c52aec2ea7663b1595a1596b29b1265
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-05-12 18:46:33 -0400

    Add files via upload

---
+++ Archive/Topalian_Address_Book_001/data/info.js
@@ -0,0 +1,98 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// info.js
+
+let info = [
+{
+    name: "James",
+    phoneCell: "555-555-5551",
+    address: "17 Galaxy Lane",
+    city: "Long Beach",
+    state: "California",
+    category: "family",
+    rating: 4,
+    date: "05/10/1980 07:17 AM"
+},
+
+{
+    name: "Zoe",
+    phoneCell: "555-555-5552",
+    address: "234 Oak Road",
+    city: "Orlando",
+    state: "Florida",
+    category: "family",
+    rating: 3,
+    date: "07/05/2022 09:00 AM"
+},
+
+{
+    name: "Sandra",
+    phoneCell: "555-555-5553",
+    address: "132 River Street",
+    city: "Bonita Springs",
+    state: "Florida",
+    category: "friend",
+    rating: 5,
+    date: "08/05/1990 05:00 PM"
+},
+
+{
+    name: "Brandi",
+    phoneCell: "555-555-5554",
+    address: "22 Ocean Ave",
+    city: "Garden Grove",
+    state: "California",
+    category: "friend",
+    rating: 5,
+    date: "09/08/2023 1:00 AM"
+},
+
+{
+    name: "Repair Shop Central",
+    phoneCell: "555-555-5555",
+    address: "217 Main Street",
+    city: "Riverside",
+    state: "California",
+    category: "mechanic",
+    rating: 2,
+    date: "02/02/1993 12:17 PM"
+},
+
+{
+    name: "Donald",
+    phoneCell: "555-555-5556",
+    address: "77 University Road",
+    city: "Orlando",
+    state: "Florida",
+    category: "friend",
+    rating: 2,
+    date: "05/20/1997 02:20 PM"
+},
+
+{
+    name: "Jane",
+    phoneCell: "555-555-5557",
+    address: "107 Sky Road",
+    city: "Paris",
+    state: "Texas",
+    category: "friend",
+    rating: 4,
+    date: "10/25/1997 04:00 PM"
+},
+
+{
+    name: "Jennifer",
+    phoneCell: "555-555-5558",
+    address: "117 Sky Road",
+    city: "Paris",
+    state: "Texas",
+    category: "friend",
+    rating: 5,
+    date: "10/25/1997 04:00 PM"
+},
+
+];
